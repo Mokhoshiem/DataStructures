1db66e9e1e7c31a24f7915b5f1d7e3722549bcac
Mohamed.Abokhoshiem <mohamed.abokhoshiem@gmail.com>
2024-08-15 19:45:28 +0300

    Add inline insertion sort

--- Algorithms/sorting_algorithms/insertion_sort.py
+++ Algorithms/sorting_algorithms/insertion_sort.py
@@ -14,8 +14,24 @@ def insertion_sort(arr: list):
                 sorted_array.append(element)
     return sorted_array
 
+def inline_insertion_sort(arr:list):
+    for element in arr:
+        for i,el in enumerate(arr):
+            # print(f'Comparing: {element} To: {el} index of {i}')
+            if element <= el:
+                popped_element = arr.pop(arr.index(element))
+                arr.insert(i,popped_element)
+                break
 
 if __name__ == '__main__':
-    arr = [10,2,5,3,1,10]
-    arr_sorted = insertion_sort(arr)
-    print(arr_sorted)
+    arrs = [[10,2,5,3,1,10],
+            [],
+            [5],
+            [-1,-2,5,2],
+            [1,2,3,4,5,6]]
+    # arr_sorted = insertion_sort(arr)
+    # print(arr_sorted)
+    for arr in arrs:
+        inline_insertion_sort(arr)
+    print(arrs)
+
